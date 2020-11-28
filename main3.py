1be6cd3ed6397ee8fec1ccd7966779100fcaad1e
TheUlr1ch <75154904+TheUlr1ch@users.noreply.github.com>
2020-11-28 08:52:12 +0300

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,31 @@
+# 1-е задание
+myList = [1, 2, 3]
+myNewList = [i * 2 для i в myList]
+print(myNewList)
+
+
+# 2-е задание
+myList = [1, 2, 3]
+myNewList = [i * * 2 для i в myList]
+print(myNewList)
+
+
+
+
+# 3-е задание
+list = 'Hello world'
+для i в диапазоне(len(list)):
+    if list[i] == " ":
+        список = список.заменить("w", "W")
+печать(список)
+
+
+# 4-е задание
+из datetime импорт datetime
+
+год1 = дата-время .сейчас().год
+год2 = 1900
+ren = год1 - год2
+для i в диапазоне(ren+1):
+    печать(год2 + i)
+
